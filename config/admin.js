58a83f47531a0551ee84752761ce4068f4bf87dd
tomislav.markovica <tomislav.markovica@em2.hr>
2022-12-08 11:38:23 +0100

    .env removed from .gitignore

--- config/admin.js
+++ config/admin.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET', ['82228246c46799b909394dc97cee09eff75e8e795df2da2913fe4bbb96e5beea']),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT', ['G+bHg+IJL9uIhyx7Cm3+xg==']),
   },
 });
