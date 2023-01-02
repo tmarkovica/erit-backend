2c361913e80b386056589b264324d1ab4b98cd46
tmarkovica.erit <tomislav@erit.hr>
2023-01-02 19:06:19 +0100

    url added to server.js

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env("URL", "http://localhost:1337"),
   app: {
     keys: env.array('APP_KEYS',
     ['qJPWGKV5mkk2styZWNAXJg==','djzMXMF5YwbrFelUUi/y5Q==','fRzhVGN9sblicSwsnBtH0A==','h8ZdFSPwYExojCWgBpFo7g==']
