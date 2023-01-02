7c09f1fda792aa4f510b2cd2391520e4d4c9bf63
tmarkovica.erit <tomislav@erit.hr>
2023-01-02 22:12:35 +0100

    edit in config/ middlewares.js

--- config/middlewares.js
+++ config/middlewares.js
@@ -12,7 +12,7 @@ module.exports = [
 ];
 
 // https://dev.to/darkmavis1980/how-to-fix-the-content-security-policy-directive-with-strapi-v4-and-upload-on-aws-s3-3bp2
-module.exports = ({ env }) => [
+/* module.exports = ({ env }) => [
   'strapi::errors',
   {
     name: 'strapi::security',
@@ -37,4 +37,27 @@ module.exports = ({ env }) => [
       },
     },
   },
+]; */
+
+// https://github.com/strapi/strapi/issues/11637#issuecomment-977244572
+module.exports = ({ env }) => [
+  'strapi::errors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        directives: {
+          'script-src': ["'self'", "'unsafe-inline'", 'cdn.jsdelivr.net'],
+          'img-src': ["'self'", 'data:', 'cdn.jsdelivr.net', 'strapi.io', `${env('AWS_BUCKET')}.s3.${env('AWS_REGION')}.amazonaws.com`],
+        },
+      }
+    },
+  },
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::favicon',
+  'strapi::public',
 ];
